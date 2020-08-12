864a62c6479a12a4ee5fc6ea85372f286b354eb0
Alexander Krahl <alexanderkrahl@hotmail.de>
2020-08-12 11:17:47 +0200

    fix build errors

--- http3/client.go
+++ http3/client.go
@@ -10,8 +10,8 @@ import (
 	"strconv"
 	"sync"
 
-	"github.com/lucas-clemente/quic-go"
-	"github.com/lucas-clemente/quic-go/internal/utils"
+	"github.com/IRelaxxx/quic-go"
+	"github.com/IRelaxxx/quic-go/internal/utils"
 	"github.com/marten-seemann/qpack"
 )
 
